4f3270a3662af8c2634b6410ea5cc292db1ca80f
Wangmeng1010 <wang1010>
2019-06-18 13:25:59 +0800

    add xiao meng

--- src/main/java/com/cn/tools/FileOp.java
+++ src/main/java/com/cn/tools/FileOp.java
@@ -1,35 +1,132 @@
 package com.cn.tools;
 
+import java.io.*;
+
 /**
+ * git commit -m "小蒙提交代码"
  * Created by wangmeng on 2019/6/14.
  */
 public class FileOp {
 
-    public static void toCsv(String fileName){
+
+   public static void toCsv(String fileName, String content) {
         // 小萌萌实现 本地文件保存
+
+        File csv = new File(fileName);
+        BufferedWriter buff = null;
+        if (csv.exists()) {
+            try {
+                csv.createNewFile();
+                buff = new BufferedWriter(new FileWriter(csv));
+                buff.write(content);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    buff.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void toExcel(String fileName) {
         return;
     }
 
-    public static void toExcel(String fileName){
+    public static void toTxt(String fileName, String content) {
+        // // 小萌萌实现 本地文件保存
+        //1创建文件
+        File file = new File("E:/test.txt");
+        if (file.exists()) {
+
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //2 write
+        //  String aa = "hello word617!";
+        try {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+            try {
+                writer.write(content);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                if (writer != null) {
+                    try {
+                        //3 close
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return;
     }
 
-    public static void toTxt(String fileName){
-        // 小萌萌实现
-        return;
-    }
-
-    public static void readCsv(String fileName){
-        // 小萌萌实现
-        return;
-    }
-
-    public static void readExcel(String fileName){
-        return;
-    }
-
-    public static void readTxt(String fileName){
+    public static String readCsv(String fileName) {
         // 小萌萌实现 本地文件读入
-        return;
+
+        String data = "";
+
+        try {
+            BufferedReader buff = new BufferedReader(new FileReader(fileName));
+            String line = "";
+            while ((line = buff.readLine()) != null) {
+
+                String item[] = line.split(",");
+                data += item[item.length - 1];
+                System.out.println("csv " + data);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public static void readExcel(String fileName) {
+
+    }
+    public static void readPDF(String fileName) {
+
+    }
+
+    public static String readTxt(String fileName) {
+        // 小萌萌实现 本地文件读入
+
+        File file = new File(fileName);
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            try {
+                fis.read();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+         FileOp fileOp = new FileOp();
+         fileOp.toCsv("E:/b.csv","李四" + "," + "198" + "," + "1992");
+         String data = FileOp.readCsv("E:/a.csv");
+         fileOp.toTxt("test.txt","hello wang");
+         fileOp.readTxt("test.txt");
     }
 }
